87fb81a17212109818396be56f22b353fe0eabb9
leonbec <dwfinz@gmail.com>
2017-12-21 16:03:57 +1300

    tablet portrait layout

--- app/src/main/java/comleonbec/github/devprofile/MainActivity.kt
+++ app/src/main/java/comleonbec/github/devprofile/MainActivity.kt
@@ -1,12 +1,20 @@
 package comleonbec.github.devprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 25f
+        logo.setImageDrawable(rounded)
     }
 }
